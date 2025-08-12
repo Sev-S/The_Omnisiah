71bce668ab7531e638098b228169da7d7ea9d9c6
tj <tilmann.loos@web.de>
2025-08-12 15:09:50 +0200

    vault backup: 2025-08-12 15:09:50

--- FH_Münster/Semester II/C++/Alklausur/Datenkrake/main.cpp
+++ FH_Münster/Semester II/C++/Alklausur/Datenkrake/main.cpp
@@ -4,14 +4,21 @@ class Vektor {
     public:
     Vektor() = default;
     Vektor(double x, double y):x{x}, y{y} {}
-    Vektor operator+ (const Vektor &other) { Vektor out;
+    Vektor operator+ (const Vektor &other) {
+        Vektor out;
         out.x = this->x * other.x;
         out.y = this->y * other.y;
         return out;
     }
-    Vektor operator* (const Vektor &other) { Vektor out;
+    /*Vektor operator* (const Vektor &other) {
+        Vektor out;
         out.y = this->x + other.x;
         out.x = this->y + other.y;
+        return out;
+    } */
+    double operator* (const Vektor &other) {
+        double out {0.0};
+        out = this->x * other.x + this->y * other.y;
         return out;
     }
     void print() { cout << x << " " << y << endl; }
@@ -20,6 +27,8 @@ private:
     double y {0.0};
 };
 int main() {
-    Vektor result = Vektor(1.3, 2.0) + Vektor(1.4, 1.0) * Vektor(1.0, 1.0);
-    result.print(); return 0;
+    //Vektor result = Vektor(1.3, 2.0) +
+       double result = Vektor(1.4, 1.0) * Vektor(1.0, 1.0);
+    cout << result << endl;
+    return 0;
 }
