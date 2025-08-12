28ef420668c360eb283f9434d61f20545b48bd29
tj <tilmann.loos@web.de>
2025-08-12 14:51:47 +0200

    vault backup: 2025-08-12 14:51:47

--- FH_Münster/Semester II/C++/Alklausur/Datenkrake/main.cpp
+++ FH_Münster/Semester II/C++/Alklausur/Datenkrake/main.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
-
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+using namespace std;
+class Vektor {
+    public:
+    Vektor() = default;
+    Vektor(double x, double y):x{x}, y{y} {} Vektor operator+ (const Vektor &other) { Vektor out; out.x = this->x * other.x;
+        out.y = this->y * other.y; return out; } Vektor operator* (const Vektor &other) { Vektor out; out.y = this->x + other.x;
+        out.x = this->y + other.y; return out; } void print() { cout << x << " " << y << endl; } private: double x {0.0}; double y {0.0};
+};
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
-
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+    Vektor result = Vektor(1.3, 2.0) + Vektor(1.4, 1.0) * Vektor(1.0, 1.0);
+    result.print(); return 0;
 }
