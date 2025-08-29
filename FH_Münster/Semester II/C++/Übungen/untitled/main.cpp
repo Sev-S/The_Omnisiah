39f938c56e26027c4b333b6b2d04e42f16e913c3
tj <tilmann.loos@web.de>
2025-08-29 17:59:46 +0200

    vault backup: 2025-08-29 17:59:46

--- FH_Münster/Semester II/C++/Übungen/untitled/main.cpp
+++ FH_Münster/Semester II/C++/Übungen/untitled/main.cpp
@@ -1,12 +1,20 @@
 #include <set>
+#include <string>
 
 class Person;
 
 class Ort {
-private:
-    std::set<Person*> Bewohner;
+    private:
+        std::set<Person*> bewohner;
     public:
-    void add_bewohner(Person* person) {
-        
+        void add_bewohner(Person* person) {
+            bewohner.insert(person);
     }
+};
+
+class Person {
+    private:
+        std::set wohnort;
+    public:
+        void add_wohnort(Wohnort* wohnort)
 };
