bf068a0159cdf1016d57fb0c5b326235a25e3ede
tj <tilmann.loos@web.de>
2025-09-01 16:23:51 +0200

    vault backup: 2025-09-01 16:23:51

--- FH_Münster/Semester II/C++/Übungen/untitled2/main.cpp
+++ FH_Münster/Semester II/C++/Übungen/untitled2/main.cpp
@@ -4,7 +4,12 @@ class Person {
 private:
     std::string name;
 public:
+    Person(std::string name) : name(name) {};
     std::string get_name() {
         return name;
     };
+    bool check_
 };
+
+Person realElvis(std::string elvis);
+Person fakeElvis(std::string elvis);
