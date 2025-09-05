75541f36063d82a216643429086a7964e584254a
tj <tilmann.loos@web.de>
2025-09-05 14:32:37 +0200

    vault backup: 2025-09-05 14:32:37

--- FH_Münster/Semester II/C++/Uebungen/Unsinn/main.cpp
+++ FH_Münster/Semester II/C++/Uebungen/Unsinn/main.cpp
@@ -1,6 +1,11 @@
+#include <print>
+
 class Quadrat {
     public:
         Quadrat(int groesse) : groesse(groesse) {};
+    void zeichnen() {
+        std::print("Hallo ich bin ein Quadrat der Größe {}", groesse);
+    }
     private:
         int groesse;
 };
